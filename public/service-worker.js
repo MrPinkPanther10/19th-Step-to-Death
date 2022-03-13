e090874676cd0020de43d7efa3f464130dd1631b
James Dabajo <90013822+MrPinkPanther10@users.noreply.github.com>
2022-03-13 14:11:29 -0400

    fixed filenames in manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,12 +2,12 @@ const FILES_TO_CACHE = [
     "/",
     "manifest.webmanifest",
     "index.html",
-    "assets/css/styles.css",
-    "assets/js/index.js",
+    "css/styles.css",
+    "js/index.js",
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
-    "assets/js/idb.js",
-    "assets/images/icons/icon-192x192.png",
-    "assets/images/icons/icon-512x512.png",
+    "js/idb.js",
+    "icons/icon-192x192.png",
+    "icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
